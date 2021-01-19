0a5665c0b0bf1fa55ad12962ecaf1c89b3ba7259
JSG <jsg9147@gmail.com>
2021-01-19 12:17:52 +0900

    포톤 서버 코드 붙여넣기 및 일부 작성

--- Assets/Scripts/LoginScene.cs
+++ Assets/Scripts/LoginScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoginScene : MonoBehaviour
+public class LoginScene : BaseScene
 {
     public void LoginButton()
     {
